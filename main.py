0d0eeb4e7718f98ad4ad6947a2d81f857115cd2f
Ruther <fedebenelli@outlook.com>
2019-08-01 11:19:13 -0300

    Format

--- main.py
+++ main.py
@@ -6,48 +6,55 @@ from datetime import datetime
 import github
 from github import Github
 import logging
-#ELO Algorithm taken from https://blog.mackie.io/the-elo-algorithm
+# ELO Algorithm taken from https://blog.mackie.io/the-elo-algorithm
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 # Open config file
 with open("config.json") as f:
-        configFile = json.load(f)
+    configFile = json.load(f)
 
 # Definitions
 botToken = configFile['botToken']
 filesToken = configFile['filesToken']
 modID = int(configFile['modID'])
 
-#In the case the bot files are hosted in a gist, functions getFile() and updateFile() also have the equivalent code for gists
+# In the case the bot files are hosted in a gist, functions getFile() and updateFile() also have the equivalent code for gists
 "gistToken = configFile['gistToken']"
 "gistClient = Github(gistToken)"
 "gist = gistClient.get_gist(filesToken)"
 
 bot = commands.Bot(command_prefix=(','), case_insensitive=True)
 
-#Events:
+
+
+
+# Events:
 
 @bot.event
 async def on_ready():  # Prints when the bot is ready
-	print('Bot is listo wacho')
-	game = discord.Game("Use ,h for commands")
-	await bot.change_presence(status=discord.Status.idle, activity=game)
+    print('Bot is listo wacho')
+    game = discord.Game("Use ,h for commands")
+    await bot.change_presence(status=discord.Status.idle, activity=game)
 
 
-#Functions:
 
-def getFile(filename):    
 
-    #Just a simpler way to open the files
+# Functions:
+
+def getFile(filename):
+
+    # Just a simpler way to open the files
     """gist = gistClient.get_gist(filesToken)
     f = (gist.files[filename].content)
     fJson = json.loads(f)"""
-    
+
     filetype = ''
     boolean = False
     for i in filename:
@@ -62,8 +69,9 @@ def getFile(filename):
             f = json.load(feed)
     return f
 
+
 def updateFile(filename, data):
-    #Just a simpler way to write on the files
+    # Just a simpler way to write on the files
     """gist.edit(description=gist.description,files={filename: github.InputFileContent(content=json.dumps(jsonData))},) #here "eloBOT" is the gist's name, can be changed for anything"""
     filetype = ''
     boolean = False
@@ -75,35 +83,39 @@ def updateFile(filename, data):
 
     with open(filename, "w") as w:
         if filetype == '.json':
-            json.dump(data,w)
+            json.dump(data, w)
         if filetype == '.txt':
             w.write(data)
 
-def getAvgRank(members, gameAcronym, players):  # Gets the members of a team, the game acronym and the stats data, then it calculates the average rank of that team
-    
+
+# Gets the members of a team, the game acronym and the stats data, then it calculates the average rank of that team
+def getAvgRank(members, gameAcronym, players):
+
     gameAcronym = gameAcronym.lower()
     rankList = []
 
-    #Make the list of ranks
+    # Make the list of ranks
     for member in members:
         for i in players:
             if i['id'] == str(member.id):
                 for game in i['games']:
                     if game['gameAcro'] == gameAcronym:
                         rankList.append(game['rank'])
-    
+
     mean = 0
     count = 0
-    
-    #Calculates the average/mean rank
+
+    # Calculates the average/mean rank
     for i in rankList:
-        mean+=float(i)
-        count+=1
+        mean += float(i)
+        count += 1
     mean = mean/count
     return mean
 
-def getMembers(str): # Makes a list of discord.Member's that are tagged in a message, message it's stored as string and tagged members are received as <@id>
-    
+
+# Makes a list of discord.Member's that are tagged in a message, message it's stored as string and tagged members are received as <@id>
+def getMembers(str):  
+
     boolean = False
     members = []
     aux = ""
@@ -113,19 +125,21 @@ def getMembers(str): # Makes a list of discord.Member's that are tagged in a mes
             members.append(int(aux))
             aux = ""
         if boolean:
-            aux += i    
+            aux += i
         if i == "@":
             boolean = True
     i = 0
     for member in members:
         members[i] = bot.get_user(member)
-        i+=1
+        i += 1
     return members
 
-def check(member):  # Gets a discord id and checks if that id is already on the database, if not it will add it. Gotta add to make it add depending on the game acronym 
+
+# Gets a discord id and checks if that id is already on the database, if not it will add it. Gotta add to make it add depending on the game acronym
+def check(member):  
     playerid = member.id
 
-    #Creates a string with the json format used, with default stats and blank id and playerName
+    # Creates a string with the json format used, with default stats and blank id and playerName
     initialStats = json.loads('''
     {
         "id":"",
@@ -138,21 +152,24 @@ def check(member):  # Gets a discord id and checks if that id is already on the 
     }''')
 
     playerid = str(playerid)
-    print(f'checked id: {playerid} from user: {member.name}' )
-    
+    print(f'checked id: {playerid} from user: {member.name}')
+
     # Opens stats file, saves the data as a variable
     statsJson = getFile('stats.json')
-    for i in statsJson['stats']['players']:  # Checks if the player id is in database, if it is then it returns
+    # Checks if the player id is in database, if it is then it returns
+    for i in statsJson['stats']['players']:
         if playerid == i['id']:
             return True
 
-    #If players is not in the database it adds him
+    # If players is not in the database it adds him
     initialStats['id'] = playerid
     initialStats['playerName'] = member.name
     statsJson['stats']['players'].append(initialStats)
-    updateFile('stats.json',statsJson)
+    updateFile('stats.json', statsJson)
 
-async def regStats(a, gameAcronym, membersA, membersB): # Gets the match result (win/tie), and appends the new stats file with the date and detail to the registry.json file
+
+# Gets the match result (win/tie), and appends the new stats file with the date and detail to the registry.json file
+async def regStats(a, gameAcronym, membersA, membersB):
 
     gameAcronym = gameAcronym.lower()
     data = getFile('stats.json')
@@ -186,14 +203,14 @@ async def regStats(a, gameAcronym, membersA, membersB): # Gets the match result 
 
     playerAAux = '''{"id":"","name":""}'''
     playerAAux = json.loads(playerAAux)
-	
+
     playersA = '''[]'''
     playersA = json.loads(playersA)
     for playerA in membersA:
         playerAAux['id'] = playerA.id
         playerAAux['name'] = playerA.name
         playersA.append(playerAAux)
-    
+
     playerBAux = '''{"id":"","name":""}'''
     playerBAux = json.loads(playerBAux)
 
@@ -203,48 +220,52 @@ async def regStats(a, gameAcronym, membersA, membersB): # Gets the match result 
         playerBAux['id'] = playerB.id
         playerBAux['name'] = playerB.name
         playersB.append(playerBAux)
-    
-    
+
     newInput['date'] = date
     newInput['detail'] = f"{playersA} {a} against {playersB} at {gameAcronym}"
     newInput['stats'] = players
 
     registry.append(newInput)
-	
-	#Sends the mod the new stats for backup
-    msj = "\nNew Reg!:" + "\n\n" + f"{json.dumps(playersA)} {a} against {json.dumps(playersB)} at {gameAcronym}" + "\n\nStats:\n" + json.dumps(players)
+
+    # Sends the mod the new stats for backup
+    msj = "\nNew Reg!:" + "\n\n" + \
+        f"{json.dumps(playersA)} {a} against {json.dumps(playersB)} at {gameAcronym}" + \
+        "\n\nStats:\n" + json.dumps(players)
     man = bot.get_user(modID)
     await man.send(msj)
     print(log)
     log += f"\n{playersA} {a} against {playersB} at {gameAcronym}"
     print(log)
-	#Stores new registry
+    # Stores new registry
     updateFile('registry.json', registry)
-    updateFile('log.txt',log)
+    updateFile('log.txt', log)
 
     return
 
-def deltaelo(memberA, teamBrank, gameAcronym,S, players):  # Calculates delta on ELO rank
+
+# Calculates delta on ELO rank
+def deltaelo(memberA, teamBrank, gameAcronym, S, players):  
     gameAcronym = gameAcronym.lower()
     RA = 0.
 
     # Get the actual rank of memberA
-    for i in players:   
+    for i in players:
         if str(memberA.id) == i['id']:
             for game in i['games']:
                 if game['gameAcro'] == gameAcronym:
                     RA = float(game['rank'])
 
-    #Calculation of delta elo
+    # Calculation of delta elo
     k = 100.
     n = 100.
     deltaRank = k * (S - 1 / (1 + math.pow(10.0, -(RA - teamBrank) / n)))
     return deltaRank
 
+
 def changeelo(memberA, deltaRank, gameAcronym, cond, players):
-    
+
     gameAcronym = gameAcronym.lower()
-    #change Ranks    
+    # change Ranks
     for i in players:
         if str(memberA.id) == i['id']:
             for game in i['games']:
@@ -258,10 +279,11 @@ def changeelo(memberA, deltaRank, gameAcronym, cond, players):
 
     stats = getFile('stats.json')
     stats['stats']['players'] = players
-    
-    #save new data    
+
+    # save new data
     updateFile('stats.json', stats)
-    
+
+
 def sendStats(member, gameAcronym):
 
     gameAcronym = gameAcronym.lower()
@@ -280,15 +302,15 @@ def sendStats(member, gameAcronym):
                         tm = game['TM']
                         gameName = game['gameName']
                         break
-        #Embed design
+        # Embed design
         name = f'{memberNick}\'s {gameName} Stats:'
         avatar = str(member.avatar_url)
         data = f'```\nRank:{round(float(r),3):->10}\nWins:{w:->10}\nTies:{t:->10}\nMatches:{tm:->7}```'
-        
+
         embed = discord.Embed(colour=discord.Colour.green())
         embed.set_thumbnail(url=avatar)
         embed.add_field(name=name, value=data, inline=True)
-    
+
     else:
         avatar = str(member.avatar_url)
         embed = discord.Embed(colour=discord.Colour.green())
@@ -297,22 +319,22 @@ def sendStats(member, gameAcronym):
         for i in statsJson['stats']['players']:
             if str(memberID) == i['id']:
                 for game in i['games']:
-                        r = game['rank']
-                        w = game['W']
-                        t = game['T']
-                        tm = game['TM']
-                        gameName = game['gameName']
+                    r = game['rank']
+                    w = game['W']
+                    t = game['T']
+                    tm = game['TM']
+                    gameName = game['gameName']
 
-                        name = f'{memberNick}\'s {gameName} Stats:'                        
-                        data = f'```\nRank:{round(float(r),3):->10}\nWins:{w:->10}\nTies:{t:->10}\nMatches:{tm:->7}```'
-                        embed.add_field(name=name, value=data, inline=True)      
+                    name = f'{memberNick}\'s {gameName} Stats:'
+                    data = f'```\nRank:{round(float(r),3):->10}\nWins:{w:->10}\nTies:{t:->10}\nMatches:{tm:->7}```'
+                    embed.add_field(name=name, value=data, inline=True)
 
     return embed
 
 
 
 
-#Commands:
+# Commands:
 
 @bot.command()
 async def h(ctx):
@@ -328,7 +350,7 @@ async def h(ctx):
         
         \n\n'''
     name = 'eloBOT commands\'s:'
-    embed.add_field(name=name, value=string, inline=False)    
+    embed.add_field(name=name, value=string, inline=False)
     string = '''    
     \nIn the case the you want to veto a result where you\'ve been tagged use `,veto` and they\'ll contact with you.
     \nIf an user wants to veto a match, the winner needs to provide a screenshot of the match\'s result. If no proof is given then the match never happened and the ranks will be restored to their last values, so make sure to always screenshot your ranked match results.'''
@@ -339,20 +361,26 @@ async def h(ctx):
     embed.add_field(name=name, value=string, inline=False)
     await ctx.send(embed=embed)
 
+
+# Sends Current bot's ping
 @bot.command()
-async def ping(ctx):  # Sends Current bot's ping
+async def ping(ctx):  
     await ctx.send(f'Pong! {round(bot.latency*1000)}ms')
 
+
+# Checks tagged user's stats
 @bot.command()
-async def stats(ctx, gameAcronym, member: discord.Member):  # Checks tagged user's stats
-    #await role(member)
+async def stats(ctx, gameAcronym, member: discord.Member):  
+    # await role(member)
     gameAcronym = gameAcronym.lower()
     check(member)
-    await ctx.send(embed=sendStats(member,gameAcronym))
+    await ctx.send(embed=sendStats(member, gameAcronym))
 
+
+# Takes current message author's and tagged user's stats, checks them and calculates new ones and dumps in file
 @bot.command()
-async def ranked(ctx, cond, gameAcronym):  # Takes current message author's and tagged user's stats, checks them and calculates new ones and dumps in file
-    
+async def ranked(ctx, cond, gameAcronym):
+
     #  Defines variables for later
     gameAcronym = gameAcronym.lower()
     SA = 0.5
@@ -409,8 +437,10 @@ async def ranked(ctx, cond, gameAcronym):  # Takes current message author's and 
         print(len(membersA))
         print(len(membersB))
         # Calculates Avg Rank of each team
-        teamArank = getAvgRank(membersA,gameAcronym,players) * len(membersA)/len(membersB)
-        teamBrank = getAvgRank(membersB,gameAcronym,players) * len(membersB)/len(membersA)
+        teamArank = getAvgRank(membersA, gameAcronym,
+                               players) * len(membersA)/len(membersB)
+        teamBrank = getAvgRank(membersB, gameAcronym,
+                               players) * len(membersB)/len(membersA)
 
         # Calculates the new variation of rank (delta) based on actual stats and updates
         for memberA in membersA:
@@ -424,7 +454,7 @@ async def ranked(ctx, cond, gameAcronym):  # Takes current message author's and 
                 changeelo(memberB, deltaB, gameAcronym, strB, players)
         await regStats(cond, gameAcronym, membersA, membersB)
 
-        # Send new stats        
+        # Send new stats
         await ctx.send(f"{teamA}:")
         for memberA in membersA:
             await ctx.send(embed=sendStats(memberA, gameAcronym))
@@ -433,7 +463,6 @@ async def ranked(ctx, cond, gameAcronym):  # Takes current message author's and 
             await ctx.send(embed=sendStats(memberB, gameAcronym))
     print('Ranked!')
 
-    
 
 @bot.command()
 async def veto(ctx):
@@ -452,5 +481,5 @@ async def veto(ctx):
 @commands.has_permissions(administrator=True) 
 async def mod(ctx, member: discord.Member):
     """
-    
+
 bot.run(botToken)
